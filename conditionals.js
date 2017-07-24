9cfc556c8ce4bf4bb46bd3dde2ecc009f813661a
stac <jacob.benjamin.stachowicz@gmail.com>
2017-07-24 13:42:19 +0200

    if else switch statements

---
+++ conditionals.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Jacob on 2017-07-24.
+ */
+
+
+// todo: IF and ELSE statements
+
+var one = 1;
+var ten = 10;
+// &&
+if(one == ten || one == 1){
+    console.log("TRUE STATEMENT");
+
+} else {
+    console.log("FALSE STATEMENT");
+}
+
+
+
+// todo: SWITCH statements
+
+var car = "Ferrari";
+
+switch (car){
+    case "BMW":
+        alert("THE CAR IS A BMW!");
+        break;
+    case "Ferrari":
+        alert("THE CAR IS A FERRARI!");
+        break;
+
+
+
+
+}
+
+
