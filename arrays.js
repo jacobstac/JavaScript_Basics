9b5f7a83da782ef5e7d9751456d15f4adea1d296
stac <jacob.benjamin.stachowicz@gmail.com>
2017-07-24 13:04:00 +0200

    added arrays file

---
+++ arrays.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Jacob on 2017-07-24.
+ */
+    //number
+var num = 29;
+var num2 = 27;
+// alert(num+num2)
+// "string
+
+// todo: Array
+// "first way:
+// var animals = ["bear", "horse", "dog"];
+// "second way, but not better??:
+var planets = new Array("Pluto", "Jupiter", "Earth");
+alert(planets[2])
+
+
+// not effective way to add ==> planets[3] = "mars";
+// effective way
+// planets.push("Mars")
+// Lenght of array alert(planets.length)
+// alert(planets)
+
+
+// todo: LOOPS
+// object
