1cdb513045cd53b253638eefbf6ef385c3802717
stac <jacob.benjamin.stachowicz@gmail.com>
2017-08-02 09:37:30 +0200

    testing

--- arrow_functions.js
+++ arrow_functions.js
@@ -2,16 +2,7 @@
  * Created by Jacob on 2017-08-02.
  */
 
-const monsterEvents = [
-    {type: 'attack', value: 20, target: 'hero'},
-    {type: 'sleep', target: 'cave'},
-    {type: 'eat', value: 20, target: 'smaller monsters'},
-    {type: 'attack', value: 34, target: 'hero'},
-    {type: 'pay bills', value: 12, target: 'bank'}
-]
+const add = (a,b) => {
+    return a + b
 
-
-
-
-
-
+}
