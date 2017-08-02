fb3fbe9dfbf7032743bcab1857ce8c14d7f5034e
stac <jacob.benjamin.stachowicz@gmail.com>
2017-08-02 09:06:04 +0200

    learning arrow functioms

---
+++ arrow_functions.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Jacob on 2017-08-02.
+ */
+
+const monsterEvents = [
+    {type: 'attack', value: 20, target: 'hero'},
+    {type: 'sleep', target: 'cave'},
+    {type: 'eat', value: 20, target: 'smaller monsters'},
+    {type: 'attack', value: 34, target: 'hero'},
+    {type: 'pay bills', value: 12, target: 'bank'}
+]
+
+
+
+
+
+
